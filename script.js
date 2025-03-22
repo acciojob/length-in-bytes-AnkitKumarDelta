5d5aaf6fabebc8518f7c10d1e146c9bd88d53d43
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-03-22 10:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const byteSize = (str) => {
   // write your code here
+if(str=="")return 0;
+	else{
+		let count = 0;
+		for(let i=0;i<str.length;i++){
+			count++;
+		}
+		return count;
+	}
+	
 };
 
-// Do not change the code below
+// Do not cha nge the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
